0963738fc908b2d1febf69d4fd19288d5aad3316
Ernest Cheung <ErnestCheung02@gmail.com>
2021-05-03 23:10:04 -0700

    Challenge 3 done

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 // Challenge 2
 
-async function getWeather(apikey, zip, callback) {
-	const units = 'imperial';
-	const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apikey}&units=${units}`;
-	fetch(path)
-		.then((res) => res.json())
-		.then((json) => {
-			callback(json.main.temp, json.weather[0].description);
-		})
-		.catch((err) => console.log(err.message));
-}
+// async function getWeather(apikey, zip, callback) {
+// 	const units = 'imperial';
+// 	const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apikey}&units=${units}`;
+// 	fetch(path)
+// 		.then((res) => res.json())
+// 		.then((json) => {
+// 			callback(json.main.temp, json.weather[0].description);
+// 		})
+// 		.catch((err) => console.log(err.message));
+// }
 
 // Challenge 3
 
-// async function getWeather(apikey, zip) {
-// 	return new Promise((resolve, reject) => {
-// 		const units = 'imperial';
-// 		const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apikey}&units=${units}`;
-// 		fetch(path)
-// 			.then((res) => res.json())
-// 			.then((json) => {
-// 				// callback(json.main.temp, json.weather[0].description);
-// 				return;
-// 			})
-// 			.catch((err) => console.log(err.message));
-// 	});
+// function getWeather(zip, apikey, units = 'imperial') {
+// 	const resPromise = fetch(path);
+// 	const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apikey}&units=${units}`;
+
+// 	const jsonP = resPromise.then((res) => res.json());
 // }
+
+// Challenge 4
+
+function getWeather(zip, apikey, units = 'imperial') {
+	const resPromise = fetch(path);
+	const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apikey}&units=${units}`;
+
+	const jsonP = resPromise.then((res) => res.json());
+}
 
 // module.exports = {
 // 	getWeather,
